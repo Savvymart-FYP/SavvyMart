e15f638b2eba9b046c54ac394c1822ebb6cc0168
Abdullah Bilal <Abdullahbilal938@gmail.com>
2026-02-26 15:43:32 +0500

    Data converted to the lower case

--- Model_Training/data_preprocessing.py
+++ Model_Training/data_preprocessing.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import re
 
 df = pd.read_csv(r'E:\SavvyMart\Model_Training\fake_reviews_dataset.csv')
 
-
 df['text_'] = df['text_'].str.lower()   #This line converted the review column into lower case letters
+
+
+
+
 print(df.head())
