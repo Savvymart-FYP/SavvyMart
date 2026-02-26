89d7432be1c60b5f729f1d2040622321d2d92d98
Abdullah Bilal <Abdullahbilal938@gmail.com>
2026-02-26 15:48:02 +0500

    Html Tags from the Data set removed

--- Model_Training/data_preprocessing.py
+++ Model_Training/data_preprocessing.py
@@ -6,6 +6,11 @@ df = pd.read_csv(r'E:\SavvyMart\Model_Training\fake_reviews_dataset.csv')
 df['text_'] = df['text_'].str.lower()   #This line converted the review column into lower case letters
 
 
+def remove_html_tags(text):   #This function will remove all the html tags etc from scrapping
+    pattern = re.compile('!<.*?>')
+    return pattern.sub(r'', text)
+
+df['text_'] = df['text_'].apply(remove_html_tags)
 
 
 print(df.head())
